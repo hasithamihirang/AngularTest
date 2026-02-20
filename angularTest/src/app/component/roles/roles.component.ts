b41ad56c7e4b3c41341be1c00068ad359cf6ecf7
HasithaMihiran <hmihiran@hawesandcurtis.com>
2026-02-21 01:04:20 +0530

    ang

--- angularTest/src/app/component/roles/roles.component.ts
+++ angularTest/src/app/component/roles/roles.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.scss'
 })
 export class RolesComponent {
+ firstName: string = "Tom";
+ lastName = "David";
+ age: number = 18;
+ isActive: boolean = true;
+ currentDate: Date = new Date();
+ inputType:string = "radio";
+ gender:string = '';
 
+ showWelcomeAlert(){  
+alert("Welcome!!!")
+ }
+ showMessage(message: string){  // function with parameter
+  alert(message)
+ }
 }
